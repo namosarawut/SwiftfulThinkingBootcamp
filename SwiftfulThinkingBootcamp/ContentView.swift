02bc0bcc9c17366c69e28537760272b5c7b8c63b
Sarawut Saengchan <namosarawut02@gmail.com>
2022-11-06 11:25:26 +0700

    Initial Commit

---
+++ SwiftfulThinkingBootcamp/ContentView.swift
@@ -0,0 +1,26 @@
+//
+//  ContentView.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by Namo Sarawut on 6/11/2565 BE.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ SwiftfulThinkingBootcamp/SwiftfulThinkingBootcampApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftfulThinkingBootcampApp.swift
+//  SwiftfulThinkingBootcamp
+//
+//  Created by Namo Sarawut on 6/11/2565 BE.
+//
+
+import SwiftUI
+
+@main
+struct SwiftfulThinkingBootcampApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
